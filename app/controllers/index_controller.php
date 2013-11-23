1be5fd7b320f3903d9132ef1b78a9526abf10665
Aaron Munguia <aamm89@gmail.com>
2013-11-23 12:30:30 -0600

    Index view

--- app/controllers/index_controller.php
+++ app/controllers/index_controller.php
@@ -1,30 +1,13 @@
-<?php 
- 
+<?php
 class Index_controller extends AppController{
 
 	public function __construct(){
 		parent::__construct();
 	}
 	
-	public function index($a=null,$b=null,$c=null,$d=null){		
-		echo $a . "\n";
-		echo $b . "\n";
-		echo $c . "\n";
-		echo $d . "\n";
+	public function index($pag = null){		
 		
 		$this->render();
-	}
-	
-	public function beforeDispatch(){
-		echo "beforeDispatch\n";
-	}
-	
-	public function beforeRender(){
-		echo "beforeRender\n";
-	}
-	
-	public function afterRender(){
-		echo "afterRender\n";
 	}
 }
 
--- app/views/index/index.php
+++ app/views/index/index.php
@@ -1,1 +1,4 @@
-Hello World!
+<h1>Wallet Hello World!</h1>
+
+<?php echo $this->html->linkTo('Login', "login", 'class="btn btn-primary"'); ?>
+<?php echo $this->html->linkTo('Register', "users/create", 'class="btn"'); ?>
