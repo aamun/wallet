820c54103d70eb3cf1d9faec811e2ca5c247ba6b
Aaron Munguia <aamm89@gmail.com>
2013-11-23 12:28:52 -0600

    Set layout to html5 Doctype Add resource to layout

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<!DOCTYPE html>
+<html>
 	<head>
     	<title><?php echo $title_for_layout; ?></title>
 		<meta name="generator" content="flavorPHP" />
-        <?php echo $this->html->charsetTag("UTF-8"); ?>		
+        <?php echo $this->html->charsetTag("UTF-8"); ?>
+        <?php echo $this->html->includeCSS('bootstrap.min'); ?>
+        <?php echo $this->html->includeCSS('bootstrap-theme.min'); ?>
+
+        <?php echo $this->html->includeJs('jquery-1.9.1'); ?>
 	</head>
     <body>
-        <div>
+        <div class="container">
             <?php echo $content_for_layout ?>
         </div>
     </body>
