7ae76dfa4829db0a253ce3ea6f678e27a4260a88
Aaron Munguia <aamm89@gmail.com>
2013-11-24 11:33:34 -0600

    Style login/index view

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,15 +1,22 @@
 <h1>Login</h1>
 
-<?php echo $this->html->form("login"); ?>
-	<p>
-		<label>Email:</label>
-		<input type="text" name="email" value="<?php echo $user['email']; ?>">
-	</p>
-	<p>
-		<label>Password:</label>
-		<input type="password" name="password">
-	</p>
-	<p>
-		<input type="submit" name="login" value="Login">
-	</p>
+<?php echo $this->html->form("login", 'POST', 'role="form" class="form-horizontal"'); ?>
+    <div class="form-group">
+        <label for="email" class="col-sm-2 control-label">Email:</label>
+        <div class="col-sm-10">
+            <input type="text" name="email" value="<?php echo $user['email']; ?>" class="form-control" placeholder="Email">
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="password" class="col-sm-2 control-label">Password:</label>
+        <div class="col-sm-10">
+            <input type="password" name="password" class="form-control" placeholder="password">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <input type="submit" name="login" value="Login" class="btn btn-primary">
+            <?php echo $this->html->linkTo("Sign up", "users/create", 'class="btn btn-default"'); ?>
+        </div>
+    </div>
 </form>
