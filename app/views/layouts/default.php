9905d0e817a7b4c94b35ad98166005fafb5e4c14
Aaron Munguia <aamm89@gmail.com>
2013-11-23 20:24:08 -0600

    add meta author and comment the include of bootstrap theme

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -2,10 +2,11 @@
 <html>
 	<head>
     	<title><?php echo $title_for_layout; ?></title>
+        <meta name="author" content="Aarón Munguía">
 		<meta name="generator" content="flavorPHP" />
         <?php echo $this->html->charsetTag("UTF-8"); ?>
         <?php echo $this->html->includeCSS('bootstrap.min'); ?>
-        <?php echo $this->html->includeCSS('bootstrap-theme.min'); ?>
+        <?php //echo $this->html->includeCSS('bootstrap-theme.min'); ?>
 
         <?php echo $this->html->includeJs('jquery-1.9.1'); ?>
 	</head>
