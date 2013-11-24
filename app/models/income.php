01963e522ecb98dfe388a62c26352e548fc4b483
Aaron Munguia <aamm89@gmail.com>
2013-11-24 17:31:21 -0600

    extend of AppModel override created method override save method

--- app/models/income.php
+++ app/models/income.php
@@ -2,8 +2,18 @@
 /**
 * Income Model
 */
-class Income extends Models
-{
+class Income extends AppModel {
 	
+	public function create($values){
+		$values['created'] = date("Y-m-d");
+		return parent::create($values);
+	}
+
+	public function save(){
+		if ($this['income_date'] == "") {
+			$this['income_date'] = date('Y-m-d');
+		}
+		return parent::save();
+	}
 }
 ?>
