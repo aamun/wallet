27bb94363641a3e8466e2a2e3caac75853d849a0
Aaron Munguia <aamm89@gmail.com>
2013-11-24 16:05:46 -0600

    fix panel style

--- app/views/layouts/panel.php
+++ app/views/layouts/panel.php
@@ -2,11 +2,13 @@
 <html>
 	<head>
     	<title><?php echo $title_for_layout; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         <meta name="author" content="Aarón Munguía">
 		<meta name="generator" content="flavorPHP" />
         <?php echo $this->html->charsetTag("UTF-8"); ?>
         <?php echo $this->html->includeCSS('bootstrap.min'); ?>
-        <?php echo $this->html->includeCSS('bootstrap-theme.min'); ?>
+        <?php echo $this->html->includeCSS('skin'); ?>
+        <?php //echo $this->html->includeCSS('bootstrap-theme.min'); ?>
 
         <?php echo $this->html->includeJs('jquery-1.10.2.min'); ?>
         <?php echo $this->html->includeJs('bootstrap.min'); ?>
